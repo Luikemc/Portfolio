c9adb8ea8cbb702c8e8484835f471d8086aada99
Luikemc <luikemc@gmail.com>
2023-02-04 19:05:16 -0300

    Removendo lixo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario/Formulario';
+import Projetos from './componentes/Projetos/Projetos';
+import Sobremim from './componentes/Sobremim/Sobremim';
+import Configcontatos from './componentes/Configcontato/Configcontato';
+
+
 
 function App() {
+
+  const projeto = [
+    {
+      nome: 'Projetos',
+      corPrimaria: '#57c278',
+      corSecundaria: '#025E73',
+    }
+  ]
+
+  const sobremim = [
+    {
+      nomeSobre: 'Sobre Mim',
+      corPrimariaSobre: '#0FC2C0',
+      corSecundariaSobre: '#024059',
+    }
+  ]
+
+  const contato = [
+    {
+      nomeContato: 'Contato',
+      corPrimariaContato: '#0CABA8',
+      corSecundariaContato: '#024873',
+    }
+  ]
+
+  
+  
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {  
+    console.log (colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador) }/>
+
+      {sobremim.map(sobre => <Sobremim key={sobre.nomeSobre}
+      nomeSobre={sobre.nomeSobre} 
+      corPrimariaSobre={sobre.corPrimariaSobre} 
+      corSecundariaSobre={sobre.corSecundariaSobre}
+        />)}
+
+     {projeto.map(projet => <Projetos key={projet.nome} 
+     nome={projet.nome} 
+     corPrimaria={projet.corPrimaria} 
+     corSecundaria={projet.corSecundaria}
+     />)}
+
+      
+
+      {contato.map(conta => <Configcontatos key={conta.nomeContato}
+      nomeContato={conta.nomeContato} 
+      corPrimariaContato={conta.corPrimariaContato} 
+      corSecundariaContato={conta.corSecundariaContato}
+        />)}
     </div>
   );
 }
